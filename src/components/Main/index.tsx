a9abf62be703caf37c8705a60965827dfa30d1ff
Alexandre <xandongurgel@gmail.com>
2024-11-06 15:50:24 -0300

    chore: add shadcn setup

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "../_ui/button"
+
 const Main = () => {
   return (
     <main className="flex h-[100vh] items-center justify-center bg-[#0A0A0A]">
       <h1 className="text-5xl font-bold text-white">Main Component</h1>
+      <Button>Button</Button>
     </main>
   )
 }

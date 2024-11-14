8e7978bc4cff8057552489852b864694c1881086
Alexandre <xandongurgel@gmail.com>
2024-11-14 14:53:12 -0300

    fix: desetructure isLoading in common button component

--- src/components/_ui/button.tsx
+++ src/components/_ui/button.tsx
@@ -42,16 +42,16 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, isLoading, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
         {...props}
-        disabled={props.isLoading || props.disabled}
+        disabled={isLoading || props.disabled}
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
       >
-        {props.isLoading ? (
+        {isLoading ? (
           <LoaderCircleIcon className="animate-spin" />
         ) : (
           props.children
